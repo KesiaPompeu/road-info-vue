eede23f50482ff6bbfa348d7425fef4b95135dfd
KesiaPompeu <marciok.dev@gmail.com>
2022-06-25 01:01:44 +0500

    Design changes in dashboard

--- src/views/main/Dashboard.vue
+++ src/views/main/Dashboard.vue
@@ -11,14 +11,14 @@
       <v-row>
         <v-col cols="12" lg="4" md="12" sm="12">
           <v-card-title v-if="item.weather"
-            ><v-icon large>mdi-weather-sunny</v-icon
+            ><v-icon color="black" large>mdi-weather-sunny</v-icon
             >{{ item.weather[0].main }}</v-card-title
           >
           <v-card-title v-if="item.main">
             <span class=" " style="font-size: 7rem">
               {{ item.temperature }}</span
             >
-            <v-icon>mdi-temperature-celsius</v-icon> <v-spacer></v-spacer>
+            <v-icon color="black">mdi-temperature-celsius</v-icon> <v-spacer></v-spacer>
             <span class=""
               >{{ item.maxTemperature }} <v-divider></v-divider>
               <span>{{ item.minTemperature }}</span></span
@@ -29,7 +29,7 @@
           <div class="d-flex px-10">
             <v-card width="120" color="#ffffff47" v-if="item.main">
               <v-card-title class="justify-center"
-                ><v-icon x-large>mdi-thermometer</v-icon></v-card-title
+                ><v-icon color="black" x-large>mdi-thermometer</v-icon></v-card-title
               >
               <v-card-subtitle class="text-center pb-0"
                 >Feels Like</v-card-subtitle
@@ -40,7 +40,7 @@
             </v-card>
             <v-card width="120" class="ml-5" color="#ffffff47" v-if="item.wind">
               <v-card-title class="justify-center">
-                <v-icon x-large color="">mdi-weather-windy-variant</v-icon>
+                <v-icon x-large color="black">mdi-weather-windy-variant</v-icon>
               </v-card-title>
               <v-card-subtitle class="text-center pb-0">Wind</v-card-subtitle>
               <v-card-title class="justify-center pt-0">{{
@@ -49,7 +49,7 @@
             </v-card>
             <v-card width="120" class="ml-5" color="#ffffff47" v-if="item.main">
               <v-card-title class="justify-center">
-                <v-icon x-large color="">mdi-weather-windy-variant</v-icon>
+                <v-icon x-large color="black">mdi-weather-windy-variant</v-icon>
               </v-card-title>
               <v-card-subtitle class="text-center pb-0"
                 >Humidity</v-card-subtitle
@@ -60,7 +60,7 @@
             </v-card>
             <v-card width="120" class="ml-5" color="#ffffff47" v-if="item.main">
               <v-card-title class="justify-center"
-                ><v-icon x-large color=""
+                ><v-icon color="black" x-large 
                   >mdi-weather-windy-variant</v-icon
                 ></v-card-title
               >
@@ -75,7 +75,7 @@
           <div class="d-flex px-10 mt-5">
             <v-card width="120" color="#ffffff47" v-if="item.main">
               <v-card-title class="justify-center"
-                ><v-icon x-large>mdi-thermometer</v-icon></v-card-title
+                ><v-icon color="black" x-large>mdi-thermometer</v-icon></v-card-title
               >
               <v-card-subtitle class="text-center pb-0"
                 >Distance Remaining</v-card-subtitle
@@ -86,7 +86,7 @@
             </v-card>
             <v-card width="120" class="ml-5" color="#ffffff47" v-if="item.wind">
               <v-card-title class="justify-center">
-                <v-icon x-large color="">mdi-weather-windy-variant</v-icon>
+                <v-icon color="black" x-large >mdi-weather-windy-variant</v-icon>
               </v-card-title>
               <v-card-subtitle class="text-center pb-0">Length</v-card-subtitle>
               <v-card-title class="justify-center pt-0">{{
@@ -95,7 +95,7 @@
             </v-card>
             <v-card width="120" class="ml-5" color="#ffffff47" v-if="item.main">
               <v-card-title class="justify-center">
-                <v-icon x-large color="">mdi-speedometer</v-icon>
+                <v-icon x-large color="black">mdi-speedometer</v-icon>
               </v-card-title>
               <v-card-subtitle class="text-center pb-0">Speed</v-card-subtitle>
               <v-card-title class="justify-center pt-0">{{
@@ -106,261 +106,7 @@
         </v-col>
       </v-row>
     </v-card>
-    <!-- <v-row>
-      <v-col
-        cols="12"
-        lg="12"
-        md="12"
-        sm="12"
-        v-for="(item, i) in location"
-        :key="i"
-      >
-        <v-card color="" class="mx-auto borderRadius mt-10">
-          <v-row>
-            <v-col cols="12" lg="4" md="12" sm="12" class="py-0">
-              <v-img
-                :src="require('../../assets/download.png')"
-                class="borderRadius"
-                width="100%"
-              >
-              </v-img>
-            </v-col>
-            <v-col cols="12" lg="8" md="12" sm="12" class="pa-0">
-              <v-img
-                :src="
-                  computeImage(filter(item.shape).map((x) => x.weather[0].main))
-                "
-                class="borderRadius"
-              >
-                <v-row class="pl-5 pt-5">
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar>
-                      <v-img
-                        :src="require('../../assets/distance.png')"
-                      ></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title class="white--text"
-                        >Length</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text">{{
-                        item.length
-                      }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar>
-                      <v-img
-                        :src="require('../../assets/distance.png')"
-                      ></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title class="white--text"
-                        >Distance</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text">{{
-                        item.remainDistance
-                      }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar>
-                      <v-img :src="require('../../assets/city.png')"></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-subtitle class="white--text">{{
-                        item.attributes.ROAD_GEOM_FCN[0].NAME
-                      }}</v-list-item-subtitle>
-                      <v-list-item-title class="white--text">{{
-                        item.attributes.ROAD_ADMIN_FCN[0].ISO_COUNTRY_CODE
-                      }}</v-list-item-title>
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar>
-                      <v-icon color="white" x-large>mdi-speedometer</v-icon>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title class="white--text"
-                        >Speed Limit</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text"
-                        >56 Km/h</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-                  </v-col>
-                </v-row>
-                <v-row class="pl-5 mt-0">
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar
-                      v-if="
-                        filter(item.shape)
-                          .map((x) => x.weather[0].main)
-                          .toString().length
-                      "
-                    >
-                      <v-img :src="require('../../assets/icon.png')"></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title
-                        class="white--text"
-                        v-if="
-                          filter(item.shape)
-                            .map((x) => x.weather[0].main)
-                            .toString().length
-                        "
-                        >Weather</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text">{{
-                        filter(item.shape)
-                          .map((x) => x.weather[0].main)
-                          .toString()
-                      }}</v-list-item-subtitle>
-                      <v-list-item-subtitle class="white--text">{{
-                        filter(item.shape)
-                          .map((x) => x.weather[0].description)
-                          .toString()
-                      }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar
-                      v-if="
-                        filter(item.shape)
-                          .map((x) => x.main.temp)
-                          .toString().length
-                      "
-                      width="50"
-                    >
-                      <span class="font-weight-medium white--text">
-                        {{
-                          parseFloat(
-                            filter(item.shape)
-                              .map((x) => x.main.temp)
-                              .toString() - 273.15
-                          ).toFixed(2)
-                        }}
-                      </span>
-                      <v-icon medium color="white"
-                        >mdi-temperature-celsius</v-icon
-                      >
-                    </v-list-item-avatar>
-                    <v-list-item-content
-                      v-if="
-                        filter(item.shape)
-                          .map((x) => x.main.temp)
-                          .toString().length
-                      "
-                    >
-                      <v-list-item-title class="white--text"
-                        >Temperature</v-list-item-title
-                      >
-                      <v-list-item-subtitle class="white--text">
-                        <span class="mr-5">Max</span>
-                        {{
-                          parseFloat(
-                            filter(item.shape)
-                              .map((x) => x.main.temp_max)
-                              .toString() - 273.15
-                          ).toFixed(2)
-                        }}</v-list-item-subtitle
-                      >
-                      <v-list-item-subtitle class="white--text">
-                        <span class="mr-5">Feels</span
-                        >{{
-                          parseFloat(
-                            filter(item.shape)
-                              .map((x) => x.main.feels_like)
-                              .toString() - 273.15
-                          ).toFixed(2)
-                        }}</v-list-item-subtitle
-                      >
-                      <v-list-item-subtitle class="white--text">
-                        <span class="mr-5">Min</span
-                        >{{
-                          parseFloat(
-                            filter(item.shape)
-                              .map((x) => x.main.temp_min)
-                              .toString() - 273.15
-                          ).toFixed(2)
-                        }}</v-list-item-subtitle
-                      >
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar
-                      v-if="
-                        filter(item.shape)
-                          .map((x) => x.main.humidity)
-                          .toString().length
-                      "
-                    >
-                      <v-img :src="require('../../assets/icon.png')"></v-img>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title
-                        class="white--text"
-                        v-if="
-                          filter(item.shape)
-                            .map((x) => x.main.humidity)
-                            .toString().length
-                        "
-                        >Humidity</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text">{{
-                        filter(item.shape)
-                          .map((x) => x.main.humidity)
-                          .toString()
-                      }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-col>
-                  <v-col cols="12" lg="3" md="6" sm="12">
-                    <v-list-item-avatar
-                      v-if="
-                        filter(item.shape)
-                          .map((x) => x.wind.speed)
-                          .toString().length
-                      "
-                    >
-                      <v-icon color="white" x-large>mdi-weather-windy</v-icon>
-                    </v-list-item-avatar>
-
-                    <v-list-item-content>
-                      <v-list-item-title
-                        class="white--text"
-                        v-if="
-                          filter(item.shape)
-                            .map((x) => x.wind.speed)
-                            .toString().length
-                        "
-                        >Wind</v-list-item-title
-                      >
-
-                      <v-list-item-subtitle class="white--text">{{
-                        filter(item.shape)
-                          .map((x) => x.wind.speed)
-                          .toString()
-                      }}</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-col>
-                </v-row>
-              </v-img>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row> -->
+   
   </v-container>
 </template>
 
